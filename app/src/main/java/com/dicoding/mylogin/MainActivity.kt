abff39c25e1fe2b8b5b3f39cbb4f607143f16ede
5751mn <edi.eastfield99@gmail.com>
2022-02-17 01:08:23 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/mylogin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.mylogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
